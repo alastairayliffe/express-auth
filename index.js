1f928ef7b924334954b5dc12e5373e798eee62f6
alia <alastairayliffe@Alastairs-MacBook.local>
2018-02-27 14:28:59 +0000

    first attempt

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
 const app = express();
+const bcrypt = require('bcrypt');
+
+const pgp = require('pg-promise')();
+const db = pgp({
+  host: 'localhost',
+  port: 5432,
+  database: process.env.DATABASE,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD
+})
+
+const salt = bcrypt.genSaltSync(10);
+const hash = bcrypt.hashSync('my password',salt);
+
+
+
+
 
 // create temporary storage for login data
 const storage = {
@@ -28,6 +45,7 @@ function getUserByUsername(username){
 
 app.use('/static', express.static('static'));
 app.use(bodyParser.json());
+app.set('view engine', 'hbs');
 
 // configure user session
 app.use(session({
@@ -51,16 +69,59 @@ passport.deserializeUser(function(id, done) {
 // that is use locally stored credentials
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    const user = getUserByUsername(username);
-    if (!user) return done(null, false);
-    if (user.password != password) return done(null, false);
-    return done(null, user);
+    //const user = getUserByUsername(username);
+    lookUpUser(username).then(function(user){
+      if (!user) return done(null, false);
+      if (user.password != password) return done(null, false);
+      return done(null, user);
+    });
   }
 ));
 
 // initialise passport and session
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.get('/register', function(req, res){
+  res.render('page-register');
+});
+
+
+app.post('/register', function(req, res){
+
+  let newUser = req.body;
+  app.use(passport.initialize());
+
+});
+
+function lookUpUser(user) {
+  debugger;
+  const {username, password} = user;
+  return db.one(`SELECT * FROM users WHERE username=$1`, [username])
+  .then(function(data){
+    return data;
+  })
+  .catch(function(error){
+    
+   // insertUser(user);
+  });
+
+}
+
+function insertUser(newUser){
+  const {username, password} = newUser;
+  const hashNewUser = bcrypt.hashSync(password, salt);
+  console.log(hashNewUser);
+  db.one(`INSERT INTO users(username, password) 
+      VALUES($1,$2) RETURNING id`, [username, hashNewUser])
+  .then(data => {
+    console.log('new user inserted');
+  })
+  .catch(error => {
+    console.log('could not insert');
+  });
+}
+
 
 // helper function to check user is logged in
 function isLoggedIn(req, res, next){
@@ -81,6 +142,9 @@ app.get('/profile', isLoggedIn, function(req, res){
   // send user info. It should strip password at this stage
   res.json({user:req.user});
 });
+
+
+
 
 app.listen(8080, function() { // Set app to listen for requests on port 3000
   console.log('Listening on port 8080!'); // Output message to indicate server is listening
